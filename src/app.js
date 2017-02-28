c8ae89ae06f1f8624d563748993c5086751dc0a9
Pedro Reis <pedroreisv@gmail.com>
2017-02-27 21:17:31 -0300

    add firebase init

--- src/app.js
+++ src/app.js
@@ -2,12 +2,24 @@ import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 
 import { Header } from './components/common'
+import firebase from 'firebase'
+
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCSMAp6lNAKJZYs48hnigQsPwujZiy_lGQ',
+      authDomain: 'auth-4fe13.firebaseapp.com',
+      databaseURL: 'https://auth-4fe13.firebaseio.com',
+      storageBucket: 'auth-4fe13.appspot.com',
+      messagingSenderId: '437936581017'
+    })
+  }
+
   render() {
     return (
       <View>
-        <Header headerText="Auth" />
+        <Header headerText='Auth' />
       </View>
     )
   }
