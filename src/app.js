75c092938b14d22221ddc5679f8cd2b038d1a64b
Pedro Reis <pedroreisv@gmail.com>
2017-02-28 17:18:06 -0300

    Conditionally rendering form and adding user Log Out functionality

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
 
-import { Header } from './components/common'
+import { Header, Button, Spinner, Card, CardSection } from './components/common'
 import LoginForm from './components/LoginForm'
 import firebase from 'firebase'
 
 
 class App extends Component {
+  state = { loggedIn: null }
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyCSMAp6lNAKJZYs48hnigQsPwujZiy_lGQ',
@@ -15,13 +17,47 @@ class App extends Component {
       storageBucket: 'auth-4fe13.appspot.com',
       messagingSenderId: '437936581017'
     })
+
+    firebase.auth()
+      .onAuthStateChanged((user) => {
+        if (user) {
+          this.setState({ loggedIn: true })
+        } else {
+          this.setState({ loggedIn: false })
+        }
+      })
+  }
+
+  renderContent() {
+    switch(this.state.loggedIn) {
+      case true:
+        return (
+          <Card>
+            <CardSection>
+              <Button onPress={() => firebase.auth().signOut()}>
+                Log Out
+              </Button>
+            </CardSection>
+          </Card>
+        )
+      case false:
+        return <LoginForm />
+      default:
+        return (
+          <Card>
+            <CardSection>
+              <Spinner size="large" />
+            </CardSection>
+          </Card>
+        )
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText='Auth' />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     )
   }
