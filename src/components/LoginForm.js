b6d158399ed1f9e96544ec59783bed80ecf92175
Pedro Reis <pedroreisv@gmail.com>
2017-02-28 15:59:23 -0300

    Add Input Component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
 
-import { Card, CardSection, Button } from './common'
+import { Card, CardSection, Button, Input } from './common'
 
 class LoginForm extends Component {
+  state = { email: '' }
+
   render() {
     return (
       <Card>
         <CardSection>
-          
+          <Input
+            label="Email"
+            onChangeText={email => this.setState({ email })}
+            value={this.state.email}
+            placeholder="user@example.com"
+          />
         </CardSection>
         <CardSection>
-
+          <Input 
+            label="Password"
+          />
         </CardSection>
         <CardSection>
           <Button>
